6f7a8391d1874da10fc2d5097d633fd816562718
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-07-31 01:50:24 -0400

    Revert "Update shader.c"
    This reverts commit 3f198cc763344339eb4432e89c8ac57437d093f0.

--- src/shader.c
+++ src/shader.c
@@ -5398,7 +5398,7 @@ bool spirv_patch_stereo_fs(
         if (in_func &&
             op == SpvOpImageQuerySizeLod &&
             wc >= 4 &&
-            fs_find_load(&s, in[i + 3]) >= 0)
+            fs_find_load(&s, in[i + 2]) >= 0)
         {
             uint32_t descriptor_var = 0;
             int load =
@@ -5424,8 +5424,8 @@ bool spirv_patch_stereo_fs(
                     (5u << 16) | SpvOpImageQuerySizeLod,
                     new_v3i_id,
                     id_size3,
-                    in[i + 3],   // image
-                    in[i + 4]    // lod
+                    in[i + 2],
+                    in[i + 3]
                 };
                 sb_push_n(&ob, w, 5);
             }
