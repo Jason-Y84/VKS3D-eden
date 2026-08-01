23128a5b500e87d99f0594b4c4d6c7438204929f
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-01 10:14:31 -0400

    Revert "Update shader.c"
    This reverts commit b09183ff6043fcd3ed883b4d95a449e6f5ab2328.

--- src/shader.c
+++ src/shader.c
@@ -2994,11 +2994,6 @@ fs_scan_type_instruction(
         if (dim == SpvDim2D &&
             s->n_img < FS_MAX_IMG)
         {
-            STEREO_LOG(
-                "FS_TYPEIMAGE_APPEND ptr=%p before_n_img=%u type=%u",
-                (void *)s,
-                s->n_img,
-                type_id);
             FsImageInfo *img =
                 &s->images[s->n_img++];
             memset(img, 0, sizeof(*img));
@@ -3014,11 +3009,9 @@ fs_scan_type_instruction(
             img->stereo           = (arrayed != 0);
             img->replacement_type = 0;
             STEREO_LOG(
-                "FS_ADD_IMAGE ptr=%p idx=%u id=%u n_img=%u",
-                (void *)s,
+                "FS_ADD_IMAGE idx=%u id=%u",
                 s->n_img - 1,
-                img->id,
-                s->n_img);
+                img->id);
         }
         break;
     }
@@ -3941,8 +3934,7 @@ fs_scan_instruction(
     uint32_t wc)
 {
     STEREO_LOG(
-        "FS_SCAN_STATE_BEGIN ptr=%p op=%s n_img=%u",
-        (void *)s,
+        "FS_SCAN_STATE_BEGIN op=%s n_img=%u",
         spv_op_name(op),
         s ? s->n_img : 999u);
     if (!s || !ins)
