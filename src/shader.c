c5b53de05596fa0ca377a2e4c01af2315424db58
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-06 04:59:19 -0400

    Revert "Simplify SpvOpTypeSampledImage rewrite"
    This reverts commit 4abf434558355c8def34b1717b5f9bfdf34cdd82.

--- src/shader.c
+++ src/shader.c
@@ -5594,7 +5594,41 @@ bool spirv_patch_stereo_fs(
         if (op == SpvOpTypeSampledImage &&
             wc >= 3)
         {
-            sb_push_n(&ob, &in[i], wc);
+            STEREO_LOG(
+                "FS_SAMPLED_IMAGE type=%u imageType=%u",
+                in[i + 1],
+                in[i + 2]);
+            uint32_t w[3];
+            memcpy(w, &in[i], wc * sizeof(uint32_t));
+            for (uint32_t img = 0; img < s.n_img; ++img)
+            {
+                if (s.images[img].id == w[2] &&
+                    s.images[img].stereo &&
+                    s.images[img].replacement_type)
+                {
+                    STEREO_LOG(
+                        "FS_PATCH_SAMPLED "
+                        "imageType=%u "
+                        "owner=%u "
+                        "binding=%u "
+                        "replacement=%u",
+                        w[2],
+                        s.images[img].owner_var,
+                        s.images[img].binding,
+                        s.images[img].replacement_type);
+                    w[2] = s.images[img].replacement_type;
+                    STEREO_LOG(
+                        "FS_SAMPLEDIMAGE_PATCH "
+                        "sampledImageType=%u "
+                        "oldImageType=%u "
+                        "newImageType=%u",
+                        w[1],          /* OpTypeSampledImage result id */
+                        in[i + 2],     /* original OpTypeImage */
+                        w[2]);         /* replacement OpTypeImage */
+                    break;
+                }
+            }
+            sb_push_n(&ob, w, wc);
             i += wc;
             continue;
         }
