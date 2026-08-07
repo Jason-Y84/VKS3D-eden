160dcf1bdbd48d4a587ff867158377d3bdb50fe8
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-07 06:12:52 -0400

    Emit the original pointer declaration unchanged

--- src/shader.c
+++ src/shader.c
@@ -5760,34 +5760,41 @@ bool spirv_patch_stereo_fs(
                 w[1],
                 w[2],
                 w[3]);
+            /* Always emit the original pointer declaration unchanged. */
+            sb_push_n(&ob, &in[i], wc);
+            if (in[i + 1] < id_bound)
+            {
+                emitted_type[in[i + 1]] = true;
+            }
             for (uint32_t img = 0; img < s.n_img; ++img)
             {
-                if (w[3] == s.images[img].sampled_type_id &&
-                    s.images[img].replacement_sampled_type != 0)
+                if (in[i + 3] != s.images[img].sampled_type_id ||
+                    s.images[img].replacement_sampled_type == 0 ||
+                    s.images[img].replacement_pointer_type == 0)
+                    continue;
+                memcpy(w, &in[i], wc * sizeof(uint32_t));
+                STEREO_LOG(
+                    "FS_POINTER_PATCH "
+                    "result=%u "
+                    "newResult=%u "
+                    "oldType=%u "
+                    "newType=%u "
+                    "owner=%u "
+                    "binding=%u",
+                    w[1],
+                    s.images[img].replacement_pointer_type,
+                    w[3],
+                    s.images[img].replacement_sampled_type,
+                    s.images[img].owner_var,
+                    s.images[img].binding);
+                w[1] = s.images[img].replacement_pointer_type;
+                w[3] = s.images[img].replacement_sampled_type;
+                sb_push_n(&ob, w, wc);
+                if (w[1] < id_bound)
                 {
-                    STEREO_LOG(
-                        "FS_POINTER_PATCH "
-                        "result=%u "
-                        "newResult=%u "
-                        "oldType=%u "
-                        "newType=%u "
-                        "owner=%u "
-                        "binding=%u",
-                        w[1],
-                        s.images[img].replacement_pointer_type,
-                        w[3],
-                        s.images[img].replacement_sampled_type,
-                        s.images[img].owner_var,
-                        s.images[img].binding);
-                    w[1] = s.images[img].replacement_pointer_type;
-                    w[3] = s.images[img].replacement_sampled_type;
-                    break;
+                    emitted_type[w[1]] = true;
                 }
-            }
-            sb_push_n(&ob, w, wc);
-            if (w[1] < id_bound)
-            {
-                emitted_type[w[1]] = true;
+                break;
             }
             i += wc;
             continue;
