190d2edd686b935a350b54918d4e48b0708029b3
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-14 22:05:00 -0400

    Revert tests 1+3
    This reverts commit 0125bad0b23e552cccdabe3defba35e46f181dd3.
    
    Revert "3"
    
    This reverts commit 0b1b397cfeaf6f0868a82a4eeb46213d32b862c1.

--- src/shader.c
+++ src/shader.c
@@ -5855,7 +5855,6 @@ bool spirv_patch_stereo_fs(
             uint32_t sampled_id = in[i + 1];
             uint32_t image_type = in[i + 2];
             uint32_t replacement_image = 0;
-            uint32_t replacement_sampled = 0;
             bool patch_sampled = false;
             STEREO_LOG(
                 "FS_SAMPLED_IMAGE_DECL "
@@ -5910,18 +5909,14 @@ bool spirv_patch_stereo_fs(
                     "FS_SAMPLED_IMAGE_PATCH "
                     "result=%u "
                     "oldImageType=%u "
-                    "newImageType=%u "
-                    "replacementSampled=%u",
+                    "newImageType=%u",
                     sampled_id,
                     image_type,
-                    replacement_image,
-                    replacement_sampled);
-                /*
-                 * The original sampled-image ID is being replaced by an
-                 * already-existing sampled-image type. Do not emit the old
-                 * result ID again; all pointer declarations will use the
-                 * replacement sampled-image ID.
-                 */
+                    replacement_image);
+                uint32_t w[3];
+                memcpy(w, &in[i], sizeof(w));
+                w[2] = replacement_image;
+                sb_push_n(&ob, w, 3);
                 if (sampled_id < id_bound)
                     emitted_type[sampled_id] = true;
                 i += wc;
